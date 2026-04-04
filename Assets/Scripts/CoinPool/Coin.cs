9f543dcfe2db384a53fcb053d9782764d7c4ba95
Your Name <vermadev8749@gmail.com>
2026-04-04 22:25:57 +0530

    Camera Movement added

--- Assets/Scripts/CoinPool/Coin.cs
+++ Assets/Scripts/CoinPool/Coin.cs
@@ -1,63 +1,4 @@
-﻿//using UnityEngine;
-//using static UnityEditor.Timeline.TimelinePlaybackControls;
-
-//public class Coin : MonoBehaviour
-//{
-//    private Transform player;
-//    private CarController carController;
-
-//    [SerializeField] float moveSpeed = 10f;
-//    [SerializeField] float magnetRange = 10f;
-//    private UIManager uiManager;
-//    private bool isCollected = false;
-
-//    void OnEnable()
-//    {
-//        isCollected = false;
-//    }
-//    void Start()
-//    {
-//        uiManager = FindObjectOfType<UIManager>();
-//        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
-//        if (playerObj != null)
-//        {
-//            player = playerObj.transform;
-//            carController = playerObj.GetComponent<CarController>();
-//        }
-//    }
-
-//    void Update()
-//    {
-//        if (player == null || carController == null) return;
-
-//        float distance = Vector3.Distance(transform.position, player.position);
-
-//        // 🧲 MAGNET EFFECT
-//        if (carController.IsMagnetActive() && distance < magnetRange)
-//        {
-//            Vector3 direction = (player.position - transform.position).normalized;
-
-//            transform.position += direction * moveSpeed * Time.deltaTime;
-//        }
-//    }
-
-//    private void OnTriggerEnter(Collider other)
-//    {
-//        if (isCollected) return;
-
-//        if (!other.CompareTag("Player")) return;
-
-//        Debug.Log("Coin Collected");
-
-//        isCollected = true;
-
-//        uiManager.AddCoin(1);
-
-//        gameObject.SetActive(false); // pooling friendly
-//    }
-//}
-
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
@@ -65,7 +6,7 @@ public class Coin : MonoBehaviour
     private CarController carController;
     private UIManager uiManager;
 
-    [SerializeField] float magnetRange = 10f;
+    [SerializeField] float magnetRange = 30f;
     [SerializeField] float moveSpeed = 15f;
     [SerializeField] float collectDistance = 1.5f;
 
